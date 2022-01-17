750ca769f4b1024e1f3acc8add274fad9aac0545
Arab KHEMAR <akhemar@e3k.co>
2022-01-17 16:02:23 +0100

    [MIG] update & adapt some modules for v15: - partner_validation - stock_move_list_partner - auditlog - partner_validation_sale - stock_move_list_requested_date - base_technical_features - password_security - stock_move_list_reserved - disable_odoo_online - stock_no_negative - disable_quick_create - ui_color_wasabi - google_partner_address - sale_order_search_by_client_order_ref - web_custom_label - mass_editing - stock_block_auto_purchase_order * web_search_with_and * aged_partner_balance_invoice_date

--- partner_validation_sale/models/sale_order.py
+++ partner_validation_sale/models/sale_order.py
@@ -4,18 +4,17 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import models, api, _
-from odoo.exceptions import Warning as WarningOdoo
-
+from odoo.exceptions import ValidationError
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+  _inherit = "sale.order"
 
-    @api.multi
-    def action_confirm(self):
-        restricted_partners = self.mapped('partner_id.commercial_partner_id'). \
-            filtered(lambda p: p.customer_state != 'approved'). \
-            mapped('display_name')
-        if restricted_partners:
-            raise WarningOdoo(_("The client ​%s has to be approved"
-                                " in order to confirm this sale order.") % (",".join(restricted_partners)))
-        return super().action_confirm()
+  def action_confirm(self):
+    if self.env["res.partner"]._should_check_partner_validation():
+      self._check_customer_approved_state()
+    return super().action_confirm()
+
+  def _check_customer_approved_state(self):
+    restricted_partners = (self.mapped("partner_id.commercial_partner_id").filtered(lambda p: p.customer_state != "approved").mapped("display_name"))
+    if restricted_partners:
+      raise ValidationError(_("The client %s has to be approved in order to confirm this sale order.") % (",".join(restricted_partners)))
--- stock_move_list_requested_date/__manifest__.py
+++ stock_move_list_requested_date/__manifest__.py
@@ -2,18 +2,16 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Stock Move Requested Date',
-    'version': '1.0',
-    'category': 'Stock',
-    'author': 'Numigi',
-    'maintainer': 'numigi',
-    'website': 'https://bit.ly/numigi-com',
-    'license': 'LGPL-3',
-    'description': "Add the requested date to the list of stock moves.",
-    'depends': [
-        'stock',
-    ],
-    'data': [
-        'data/ir_ui_view.xml',
-    ],
+  'name': 'Stock Move Requested Date',
+  'version': '1.0',
+  'category': 'Stock',
+  'author': 'Numigi',
+  'maintainer': 'numigi',
+  'website': 'https://bit.ly/numigi-com',
+  'license': 'LGPL-3',
+  'description': "Add the requested date to the list of stock moves.",
+  'depends': ['stock'],
+  'data': [
+    'views/stock_move_views.xml',
+  ],
 }
