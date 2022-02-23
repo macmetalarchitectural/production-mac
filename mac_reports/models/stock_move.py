6c1d4fc2fba5920edfde176bfefc4e67b315cba3
Assia Soumeur <asoumeur@e3k.co>
2022-02-23 13:03:30 -0500

    address

--- mac_reports/models/stock_move.py
+++ mac_reports/models/stock_move.py
@@ -4,16 +4,26 @@
 from odoo import fields, models, api
 from odoo.exceptions import UserError
 
-#
-# class StockMove(models.Model):
-#     _inherit = 'stock.move'
-#
-#     def get_po(self):
-#         for rec in self:
-#
-#
-#
-# class StockMoveLine(models.Model):
-#     _inherit = 'stock.move.line'
-#
-#     def get_po(self):
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def get_po(self):
+        for rec in self:
+            purchases = " "
+            for purchase in rec.sale_line_id.order_id._get_purchase_orders():
+                if rec.product_id in purchase.order_line.mapped('product_id'):
+                    purchases = purchases + " " + purchase.name
+
+
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    def get_po(self):
+        for rec in self:
+            purchases = ""
+            for purchase in rec.move_id.sale_line_id.order_id._get_purchase_orders():
+                if rec.product_id in purchase.order_line.mapped('product_id'):
+                    purchases += purchase.name
