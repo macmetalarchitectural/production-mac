d88c805f16b87b87fc1ef1b287fda95868a8f844
Assia Soumeur <asoumeur@e3k.co>
2022-02-22 22:35:48 -0500

    translation

--- mac_reports/models/sale_order.py
+++ mac_reports/models/sale_order.py
@@ -8,5 +8,10 @@ from odoo.exceptions import UserError
 class SaleOrderNote(models.Model):
     _inherit = 'sale.order'
 
+    @api.model
+    def _default_sale_note_terms(self):
+        return self.env['ir.config_parameter'].sudo().get_param(
+            'e3k_default_reports.use_sale_order_terms') and self.env.company.sale_order_terms or ''
+
     delivery_note = fields.Text('Delivery Note')
     sale_note_termes = fields.Text('Terms and conditions', default=_default_sale_note_terms, translate=True)
