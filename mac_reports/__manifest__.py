643e4f920b536fb35646605e4611e4e835c17a57
Assia Soumeur <asoumeur@e3k.co>
2022-03-17 11:11:46 -0400

    report

--- mac_reports/__manifest__.py
+++ mac_reports/__manifest__.py
@@ -11,9 +11,9 @@
     'version': '15.1',
     'depends': ['e3k_default_reports', 'account'],
     'data': [
-        'reports/sale_report.xml',
+        # 'reports/sale_report.xml',
         'reports/invoice_report.xml',
-        'reports/delivery_slip.xml',
+        # 'reports/delivery_slip.xml',
         'reports/report_picking.xml',
         'reports/purachse_order.xml',
         'views/sale_order.xml',
