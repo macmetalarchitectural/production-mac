3c038b4aa34e68f2dd4bb357391fcbfd7fc98b58
Arab KHEMAR <akhemar@e3k.co>
2022-06-02 13:55:02 +0100

    [FIX] stock_block_auto_purchase_order

--- stock_block_auto_purchase_order/models/product_template.py
+++ stock_block_auto_purchase_order/models/product_template.py
@@ -2,9 +2,10 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import api, models, fields
-from .purchase_order import BLOCK_AUTO_HELP
+from .purchase_order import BLOCK_AUTO_HELP, UNIQUE_PO_HELP
 
 class ProductTemplate(models.Model):
   _inherit = "product.template"
 
   block_auto_purchase_order = fields.Boolean(string="Block Automatic Product Add", help=BLOCK_AUTO_HELP)
+  unique_purchase_order = fields.Boolean(string="Unique Purchase Order", help=UNIQUE_PO_HELP)
--- stock_block_auto_purchase_order/models/res_partner.py
+++ stock_block_auto_purchase_order/models/res_partner.py
@@ -7,6 +7,10 @@ BLOCK_AUTO_HELP = (
   "When checked, this field prevents products from being automatically added to the PO by the system."
 )
 
+UNIQUE_PO_HELP = (
+  "When checked, this field forces items to be added to a default supplier."
+)
+
 class ResPartner(models.Model):
   _inherit = "res.partner"
 
@@ -15,3 +19,19 @@ class ResPartner(models.Model):
     default=False,
     help=BLOCK_AUTO_HELP,
   )
+
+  unique_purchase_order = fields.Boolean(
+    string="Unique Purchase Order",
+    default=False,
+    help=UNIQUE_PO_HELP,
+  )
+
+  @api.onchange("block_auto_purchase_order")
+  def onchange_block_auto_purchase_order(self):
+    if self.unique_purchase_order and self.block_auto_purchase_order:
+      self.unique_purchase_order = False
+
+  @api.onchange("unique_purchase_order")
+  def onchange_unique_purchase_order(self):
+    if self.block_auto_purchase_order and self.unique_purchase_order:
+      self.block_auto_purchase_order = False
