d1d79faae31badbce881dd358b58e278ee31d8dc
Amine Trifi <m.amine.trifi@gmail.com>
2022-07-04 13:17:31 -0400

    [UPDT] function

--- aged_partner_balance_invoice_date/models/res_partner.py
+++ aged_partner_balance_invoice_date/models/res_partner.py
@@ -6,14 +6,15 @@ class ResPartner(models.Model):
 
     def write(self, vals):
         if 'property_purchase_currency_id' in vals:
-            currency_id = self.env['res.currency'].search([('id', '=', property_purchase_currency_id)])
+            currency_id = self.env['res.currency'].search([('id', '=', vals['property_purchase_currency_id'])])
 
+            old_tag_id = self.env['res.partner.category'].search([('name', '=', self.property_purchase_currency_id.name)])
             tag_id = self.env['res.partner.category'].search([('name', '=', currency_id.name)])
 
             if not tag_id:
                 raise UserError('La devise %s est introuvable dans les étiquettes, veuillez la configurer avant la création du contact'%currency_id.name)
             else:
-                vals.update({"category_id": [[6, 0, tag_id.ids]]})
+                vals.update({"category_id": [[3, old_tag_id[0].id] if old_tag_id else False , [4, tag_id[0].id]]})
 
         result = super(ResPartner, self).write(vals)
 
