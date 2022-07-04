a4172048b6e0f0c311904611d3cdf7602323b87b
Amine Trifi <m.amine.trifi@gmail.com>
2022-07-04 13:34:06 -0400

    [UPDT] function

--- aged_partner_balance_invoice_date/models/res_partner.py
+++ aged_partner_balance_invoice_date/models/res_partner.py
@@ -4,8 +4,30 @@ from odoo.exceptions import UserError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'property_purchase_currency_id' in vals and vals['property_purchase_currency_id']:
+                currency_id = self.env['res.currency'].search([('id', '=', vals['property_purchase_currency_id'])])
+
+                old_tag_id = self.env['res.partner.category'].search(
+                    [('name', '=', self.property_purchase_currency_id.name)])
+                tag_id = self.env['res.partner.category'].search([('name', '=', currency_id.name)])
+
+                if not tag_id:
+                    raise UserError(
+                        'La devise %s est introuvable dans les étiquettes, veuillez la configurer avant la création du contact' % currency_id.name)
+                else:
+                    vals_category = []
+                    if old_tag_id:
+                        vals_category.append([3, old_tag_id[0].id], )
+                    vals_category.append([4, tag_id[0].id])
+                    vals.update({"category_id": vals_category})
+
+        return super(ResPartner, self).create(vals_list)
+
     def write(self, vals):
-        if 'property_purchase_currency_id' in vals:
+        if 'property_purchase_currency_id' in vals and vals['property_purchase_currency_id']:
             currency_id = self.env['res.currency'].search([('id', '=', vals['property_purchase_currency_id'])])
 
             old_tag_id = self.env['res.partner.category'].search([('name', '=', self.property_purchase_currency_id.name)])
@@ -14,8 +36,10 @@ class ResPartner(models.Model):
             if not tag_id:
                 raise UserError('La devise %s est introuvable dans les étiquettes, veuillez la configurer avant la création du contact'%currency_id.name)
             else:
-                vals.update({"category_id": [[3, old_tag_id[0].id] if old_tag_id else False , [4, tag_id[0].id]]})
+                vals_category = []
+                if old_tag_id:
+                    vals_category.append([3, old_tag_id[0].id],)
+                vals_category.append([4, tag_id[0].id])
+                vals.update({"category_id": vals_category})
 
-        result = super(ResPartner, self).write(vals)
-
-        return result
+        return super(ResPartner, self).write(vals)
