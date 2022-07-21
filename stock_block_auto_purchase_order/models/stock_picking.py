c54c7d7c0679716795b53785c11a8caa6e0eece9
Amine Trifi <m.amine.trifi@gmail.com>
2022-07-21 09:12:09 -0400

    [UPDT] keep native behavior

--- stock_block_auto_purchase_order/models/stock_picking.py
+++ stock_block_auto_purchase_order/models/stock_picking.py
@@ -1,9 +1,11 @@
 from odoo import fields, models, api
+import logging
+_logger = logging.getLogger(__name__)
 
 class Picking(models.Model):
     _inherit = "stock.picking"
 
-    date_deadline = fields.Datetime(compute=False)
+    date_deadline = fields.Datetime(inverse = '_set_date_deadline')
 
     def _compute_has_deadline_issue(self):
         for picking in self:
@@ -11,3 +13,17 @@ class Picking(models.Model):
                 picking.has_deadline_issue = picking.date_deadline and picking.date_deadline < picking.scheduled_date or False
             else:
                 picking.has_deadline_issue = False
+
+    def _set_date_deadline(self):
+        pass
+        # for move_line in self.move_lines:
+        #     move_line.date_deadline = self.date_deadline
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.model
+    def create(self, vals):
+        vals['date_deadline'] = False
+        res = super(StockMove, self).create(vals)
+        return res
