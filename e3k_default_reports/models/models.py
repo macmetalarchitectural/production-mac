9300b9c0d15fb21f3dc09e4d76709e3cee36b31c
Amine Trifi <m.amine.trifi@gmail.com>
2022-09-14 14:47:26 -0400

    [FIX] correction de la description multi-lignes

--- e3k_default_reports/models/models.py
+++ e3k_default_reports/models/models.py
@@ -69,6 +69,30 @@ class AccountMoveLine(models.Model):
                 for sale_line in rec.sale_line_ids:
                     qty = qty + sale_line.product_uom_qty
 
+    def _get_computed_name(self):
+        self.ensure_one()
+        if not self.product_id:
+            return ''
+
+        if self.partner_id.lang:
+            product = self.product_id.with_context(lang=self.partner_id.lang)
+        else:
+            product = self.product_id
+
+        values = [product]
+        # if product.partner_ref:
+        #     values.append(product.partner_ref)
+        if product:
+            values.append(product.name)
+        # if self.journal_id.type == 'sale':
+        #     if product.description_sale:
+        #         print(product.description_sale)
+        #         values.append(product.description_sale)
+        elif self.journal_id.type == 'purchase':
+            if product.description_purchase:
+                values.append(product.description_purchase)
+        return '\n'.join(values)
+
     def ge_commanded_qty_delivered_received(self):
         for rec in self:
             qty_delivered_received = 0
