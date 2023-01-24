fa0f410387b1e75260c9624dacc6cf393183ce6b
Amine Trifi <67431038+tmoamine@users.noreply.github.com>
2023-01-24 11:55:45 +0100

    Modifier l'adresse de livraison

---
+++ e3k_custom_sale_order/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+  "name": "e3k custom sale order",
+  "summary": "",
+  "description": "",
+  "category": "Sales/Sales",
+  "author": "e3k Solutions",
+  'license': 'LGPL-3',
+  "website": "https://e3k.co",
+  "version": "15.0.1",
+  "depends": [
+    'sale',
+  ],
+  "data": [],
+  "application": False,
+  "installable": True,
+}
