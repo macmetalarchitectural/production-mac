df725a3da5d5ef7a93dfcec74afd3bf755661341
FredericFB <fbapoungue@e3k.co>
2024-10-18 16:08:11 -0400

    [TRD] e3k_custom_stock_calendar: custom calendar

--- e3k_custom_stock_calendar/models/stock_picking.py
+++ e3k_custom_stock_calendar/models/stock_picking.py
@@ -9,9 +9,11 @@ class StockPicking(models.Model):
     delivery_pickup = fields.Boolean(string='Delivery Pickup', default=False)
     worksite_ready = fields.Boolean(string='Worksite Ready', default=False)
     flexible_date = fields.Boolean(string='Flexible Date', default=False)
+    e3k_all_day = fields.Boolean(string='All Day', default=True)
 
     @api.model
     def _action_get_value_from_x_delivery_pickup(self):
+        # fonction a executer une seule fois pour mettre a jour les valeurs
         for rec in self.search([]):
             if hasattr(rec, 'x_delivery_pickup'):
                 rec.delivery_pickup = rec.x_delivery_pickup
