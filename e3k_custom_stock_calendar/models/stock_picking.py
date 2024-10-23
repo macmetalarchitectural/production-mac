31a910c1eb1ddcb2f3984836fd88b49d6c76a30e
FredericFB <fbapoungue@e3k.co>
2024-10-23 14:33:35 -0400

    [TRD] e3k_custom_stock_calendar: Test custom name in calendar

--- e3k_custom_stock_calendar/models/stock_picking.py
+++ e3k_custom_stock_calendar/models/stock_picking.py
@@ -10,6 +10,7 @@ class StockPicking(models.Model):
     worksite_ready = fields.Boolean(string='Worksite Ready', default=False)
     flexible_date = fields.Boolean(string='Flexible Date', default=False)
     e3k_all_day = fields.Boolean(string='All Day', default=True)
+    e3k_custom_display_name = fields.Char(compute='_compute_e3k_custom_display_name', store=True)
 
     @api.model
     def _action_get_value_from_x_delivery_pickup(self):
@@ -17,4 +18,10 @@ class StockPicking(models.Model):
         for rec in self.search([]):
             if hasattr(rec, 'x_delivery_pickup'):
                 rec.delivery_pickup = rec.x_delivery_pickup
+
+    @api.depends('partner_id', 'date_deadline', 'rec.partner_id.city')
+    def _compute_e3k_custom_display_name(self):
+        for rec in self:
+            sep = ' - '
+            rec.e3k_custom_display_name = f"{rec.partner_id.name}  {sep + rec.date_deadline .strftime('%d/%m/%Y') if rec.scheduled_date else ''}  { sep + rec.partner_id.city if rec.partner_id.city else ''}"
 
