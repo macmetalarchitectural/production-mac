e1648bc5e006b364e13703b28f42648ac6a03407
FredericFB <fbapoungue@e3k.co>
2024-12-12 19:13:30 -0500

    [MEP] e3k_macmetal, e3k_mac_stock_calendar:   Calendrier de livraisons

--- e3k_macmetal/models/sale_order.py
+++ e3k_macmetal/models/sale_order.py
@@ -6,11 +6,16 @@ class SaleOrder(models.Model):
   _inherit = "sale.order"
 
   delivery_route = fields.Selection([
-    ('Route1', 'Route 1 - Laurentides, Laval, Lanaudière'),
-    ('Route2', 'Route 2 - Estrie, Rive-Sud'),
-    ('Route3', 'Route 3 - Montréal'),
-    ('Route4', 'Route 4 - Autres')], string='Delivery Route', readonly=True, help='Preferred delivery route.',
-    states={'draft': [('readonly', False)], 'sent': [('readonly', False)], 'sale': [('readonly', False)]})
+    ('Route1', 'Livraison MAC'),
+    ('Route2', 'Livraison GMR'),
+    ('Route3', 'Livraison Lacroix'),
+    ('Route4', 'Livraison - Autre'),
+    ('Route5', 'Pick up'),
+    ('Route6', 'Target'),], string='Delivery Route', help='Preferred delivery route.',)
+
+  delivery_route_id = fields.Many2one('mac.route.config', string='Delivery Route', help='Preferred delivery route.',)
+    # states={'draft': [('readonly', False)], 'sent': [('readonly', False)], 'sale': [('readonly', False)]})
 
   customer_delivery_date = fields.Datetime('Delivery Date', copy=False,
     help="This is the delivery date selected by the customer.")
+
