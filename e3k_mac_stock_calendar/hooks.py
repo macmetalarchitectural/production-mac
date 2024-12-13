ddb663d0927e4e7de78036bc4f06e2f87de5d77a
FredericFB <fbapoungue@e3k.co>
2024-12-12 19:26:40 -0500

    [MEP] e3k_macmetal, e3k_mac_stock_calendar:   Calendrier de livraisons

--- e3k_mac_stock_calendar/hooks.py
+++ e3k_mac_stock_calendar/hooks.py
@@ -4,7 +4,7 @@ from . import models
 
 def _post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env["stock.picking"].search([]).mapped("_copy_val_from_x_delivery_pickup")
+    env["stock.picking"].search([])._copy_val_from_x_delivery_pickup()
 
     view_ids_to_delete = [1975, 1976]
     env["ir.ui.view"].browse(view_ids_to_delete).unlink()
