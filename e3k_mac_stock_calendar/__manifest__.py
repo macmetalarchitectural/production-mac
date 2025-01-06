e8c8a7316171565c2c85328cbaadb1a826699a9e
FredericFB <fbapoungue@e3k.co>
2025-01-06 12:14:11 -0500

    [UPD] increase the  manifest version to  update modules automatically and fiz  warnings

--- e3k_mac_stock_calendar/__manifest__.py
+++ e3k_mac_stock_calendar/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'E3K Stock Calendar',
-    'version': '1.0.3',
+    'version': '1.0.4',
     'author': 'e3k solutions',
     'maintainer': 'e3k',
     'website': 'https://www.e3k.co/',
