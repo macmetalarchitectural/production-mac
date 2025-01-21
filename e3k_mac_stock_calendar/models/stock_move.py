b8bcbe8143b14c4479588076be8beb8766191b03
FredericFB <fbapoungue@e3k.co>
2025-01-21 09:37:34 -0500

    [fix] update  stock move date_deadline after changing in stock.picking

--- e3k_mac_stock_calendar/models/stock_move.py
+++ e3k_mac_stock_calendar/models/stock_move.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
     date_deadline = fields.Datetime(
         "Deadline", readonly=True,
-        compute='_e3k_compute_date_deadline',
+        related='picking_id.date_deadline',
         help="Date Promise to the customer on the top level document (SO/PO)")
-
-
-    @api.depends('picking_id', 'picking_id.date_deadline')
-    def _e3k_compute_date_deadline(self):
-        for move in self:
-            if move.picking_id and move.picking_id.sale_id:
-                move.date_deadline = move.picking_id.date_deadline
