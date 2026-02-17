0baf9a8b99f17db5fe75aad0d33fcb1d7fcf38c2
FredericFB <fbapoungue@e3k.co>
2026-02-17 13:28:11 -0500

    [#54470] [REM] e3k_custom_cheque: remove custom cheque module
    - Deleted the `e3k_custom_cheque` module completely.
    - Removed related translation files, models, reports, and business logic.
    - Cleaned up redundant assets and obsolete codebase.
    
    Signed-off-by: FredericFB <fbapoungue@e3k.co>

--- e3k_default_reports/models/res_company.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
-
-    b_logo = fields.Binary(string="Becorp Logo", readonly=False)  # no-check
-    sale_order_terms = fields.Text(string='Default Sale Terms and Conditions', translate=True)  # no-check
