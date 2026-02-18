8ab0097f86373c8fa95fc509ad41b083275d36f9
FredericFB <fbapoungue@e3k.co>
2026-02-18 17:20:30 -0500

    [#54510] disable_quick_create: add module to restrict quick create and edit options
    - Introduced the `disable_quick_create` module to globally disable quick create and configure "Create and Edit" options per model.
    - Added model-specific configuration via a new field `disable_create_edit` on the `ir.model` model.
    - Patched Many2One fields to dynamically enforce these restrictions based on model settings.
    - Enhanced developer experience with debugging tools for tracing component lifecycle and property changes.
    - Included new views, JavaScript assets, and French translations for seamless integration.
    
    Signed-off-by: FredericFB <fbapoungue@e3k.co>

---
+++ disable_quick_create/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2017 Savoir-faire Linux
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/LGPL).
+
+{
+  'name': 'Disable Quick Create',
+  'version': '19.0.1.0.0',
+  'author': 'Savoir-faire Linux',
+  'maintainer': 'Numigi',
+  'website': 'https://www.numigi.com',
+  'license': 'LGPL-3',
+  'category': 'Web',
+  'summary': 'Disable "quick create" for all and "create and edit for specific models',
+  'depends': ['web',],
+  'data': [
+    'views/ir_model.xml',
+  ],
+  "assets": {
+    'web.assets_backend': [
+      'disable_quick_create/static/src/views/fields/many2one_create_restrictions.js',
+    ],
+  },
+  'installable': True,
+}
