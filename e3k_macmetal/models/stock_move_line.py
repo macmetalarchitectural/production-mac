d1950e9ce4ef164a6cfd36a02d517be612e1bbfc
ouknada <noukaci@e3k.co>
2026-07-05 23:00:34 +0100

    [UPD] e3k_macmetal

--- e3k_macmetal/models/stock_move_line.py
+++ e3k_macmetal/models/stock_move_line.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockMoveLine(models.Model):
@@ -9,19 +9,7 @@ class StockMoveLine(models.Model):
     note = fields.Char('Note')  # no-check
     e3k_lot_ref = fields.Char(
         string='Internal Reference',
-        compute='_compute_e3k_lot_ref',
-        inverse='_inverse_e3k_lot_ref',
     )
-
-    @api.depends('lot_id.ref')
-    def _compute_e3k_lot_ref(self):
-        for line in self:
-            line.e3k_lot_ref = line.lot_id.ref
-
-    def _inverse_e3k_lot_ref(self):
-        for line in self:
-            if line.lot_id:
-                line.lot_id.ref = line.e3k_lot_ref
 
     def _prepare_new_lot_vals(self):
         vals = super()._prepare_new_lot_vals()
